619d04c67d63ac82acffc26bd8229ce0a2fba815
Federico Palomero <federico.palomero@globant.com>
2018-11-05 20:23:13 -0300

    added hours to the component

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -3,12 +3,21 @@
     <b-card class="text-center">
       <b-form @submit="onSubmit" @reset="onReset" v-if="show">
         <b-form-group id="exampleInputGroup3"
-                      label="How many employees?:"
+                      label="How many employees ?:"
                       label-for="exampleInput3">
           <b-form-select id="exampleInput3"
                         :options="employees"
                         required
                         v-model="form.employee">
+          </b-form-select>
+        </b-form-group>
+        <b-form-group id="exampleInputGroup3"
+                      label="At what time ?:"
+                      label-for="exampleInput3">
+          <b-form-select id="exampleInput3"
+                        :options="hours"
+                        required
+                        v-model="form.hour">
           </b-form-select>
         </b-form-group>
       </b-form>
@@ -21,11 +30,16 @@ export default {
   data () {
     return {
       form: {
+        hour: null,
         employee: null
       },
       employees: [
-        { text: 'Select One', value: null },
+        { text: 'Select the number of employees', value: null },
         '1', '2', '3'
+      ],
+      hours: [
+        { text: 'Select hour', value: null },
+        '18:00', '19:00', '20:00'
       ],
       show: true
     }
@@ -39,6 +53,7 @@ export default {
       evt.preventDefault();
       /* Reset our form values */
       this.form.employee = null;
+      this.form.hour = null;
       /* Trick to reset/clear native browser form validation state */
       this.show = false;
       this.$nextTick(() => { this.show = true });
